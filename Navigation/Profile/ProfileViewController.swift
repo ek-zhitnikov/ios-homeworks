13ba5e50689586ca39c8af06bc392ee9bf6cc635
EKTEST <e_zhitnikov@bk.ru>
2023-02-27 17:55:00 +0400

    Создал экземпляр класса ProfileHeaderView в классе ProfileViewController, добавил его в качестве subview и в методе viewWillLayoutSubviews() задал ему frame, равный frame корневого view

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,23 +8,31 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    //Создайте экземпляр класса ProfileHeaderView
+    let profileHeaderView = ProfileHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //добавьте profileHeaderView в качестве subview
+        self.view.addSubview(profileHeaderView)
+        
+        // Изменение цвета
         self.view.backgroundColor = UIColor.lightGray
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //в методе viewWillLayoutSubviews() задайте ему frame, равный frame корневого view
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let headerViewFrame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        
+        profileHeaderView.frame = headerViewFrame
     }
-    */
-
 }
+    
+
+
+
